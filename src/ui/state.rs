0f8e9a96d469c37f01bc469bb9890055a1230afd
Maciej Pędzich <contact@maciejpedzi.ch>
2024-11-16 19:39:55 +0100

    Implement passing program path via a CLI flag

--- src/ui/state.rs
+++ src/ui/state.rs
@@ -1,22 +1,43 @@
 use crate::vm::VirtualMachine;
 use async_mutex::Mutex as AsyncMutex;
+use clap::Parser;
 use notan::prelude::*;
-use rfd::FileHandle;
+use std::ffi::OsString;
 use std::path::PathBuf;
 use std::sync::Arc;
 
 #[derive(AppState)]
 pub struct State {
     pub last_dir: PathBuf,
-    pub file_handle_option: Arc<AsyncMutex<Option<FileHandle>>>,
+    pub file_handle_option: Arc<AsyncMutex<Option<PathBuf>>>,
     pub cycles_per_frame: u32,
     pub vm: VirtualMachine,
 }
 
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, default_value_os_t)]
+    file: OsString,
+}
+
 pub fn setup(_gfx: &mut Graphics) -> State {
+    let args = Args::parse();
+
+    let file_path = if args.file.is_empty() {
+        None
+    } else {
+        Some(PathBuf::from(args.file))
+    };
+
+    let last_dir = if let Some(path) = &file_path {
+        path.parent().unwrap().to_path_buf()
+    } else {
+        std::env::current_dir().unwrap()
+    };
+
     State {
-        last_dir: std::env::current_dir().unwrap(),
-        file_handle_option: Arc::new(AsyncMutex::new(None)),
+        last_dir,
+        file_handle_option: Arc::new(AsyncMutex::new(file_path)),
         cycles_per_frame: 10,
         vm: VirtualMachine::new(),
     }
@@ -26,13 +47,10 @@ pub fn update(_app: &mut App, state: &mut State) {
     let fho_guard = state.file_handle_option.try_lock();
 
     if let Some(mut fho_guard) = fho_guard {
-        if let Some(file_handle) = fho_guard.as_ref() {
-            let file_path = file_handle.path();
-
+        if let Some(file_path) = fho_guard.as_ref() {
             state.last_dir = file_path.parent().unwrap().to_path_buf();
             state.vm.reset();
             state.vm.load_program(file_path);
-
             *fho_guard = None;
         }
     };
